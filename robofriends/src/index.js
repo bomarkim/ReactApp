52283b37adfa07b64b4a9e421a606f329ec63baa
MMStechs <helpdesk@dynamic-10-125-166-202.dynamic.unt.edu>
2018-05-25 16:53:11 -0500

    started project, first test component done.

---
+++ robofriends/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Hello from './Hello';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+ReactDOM.render(<Hello greeting = {'Hello' + 'React Ninja'}/>, document.getElementById('root'));
+registerServiceWorker();
