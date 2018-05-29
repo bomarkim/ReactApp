a821cc4cd322b5cef58fbafe083bbe8efbe9fc1a
Bobby Kim <bomarkim3@gmail.com>
2018-05-29 17:20:36 -0500

    Got the cards working, and deleted the hello sections because it was not needed

---
+++ robofriends/src/Robots.js
@@ -0,0 +1,32 @@
+export const robots =[
+{
+	id: 1,
+	name: 'Frankie Boi',
+	username: 'FrankBoi',
+	email: 'Frank@Boi.biz'
+},
+{
+	id: 2,
+	name: 'Samantha Sally',
+	username: 'So_Samanth',
+	email: 'Samantha@Sally.biz'
+},
+{
+	id: 3,
+	name: 'Grendall the Monkey',
+	username: 'Bananaz 4 U',
+	email: 'Yellow@ape.monk'
+},
+{
+	id: 4,
+	name: 'Mr. Potato Head',
+	username: 'NotYetMashed',
+	email: 'ScallopedTates@unite.org'
+},
+{
+	id: 5,
+	name: 'Orange OCleaf',
+	username: 'iTheOrange',
+	email: 'VitaminCUL8ter@busta.com'
+},
+]
